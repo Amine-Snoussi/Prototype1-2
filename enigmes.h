6d5e4a7932de23774d8b250e012524732b0a893f
Amine Snoussi <42628097+Siki911@users.noreply.github.com>
2020-06-11 16:21:19 +0200

    Add files via upload

---
+++ enigmes.h
@@ -0,0 +1,40 @@
+#ifndef ENIGMES_H_INCLUDED
+#define ENIGMES_H_INCLUDED
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+struct enigmes
+
+{  SDL_Surface *image;
+
+   SDL_Rect p;
+};
+typedef struct enigmes enigme;
+
+
+
+void initialiser_enigmes1(enigme t1[],enigme t2[],int t3[]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int afficher(enigme t1[],enigme t2[],SDL_Surface *screen);
+int resolution(enigme t1[],enigme t2[]);
+
+
+#endif // ENIGMES_H_INCLUDED
---
+++ min2.h
@@ -0,0 +1,36 @@
+#ifndef FONCTION2_H_
+
+#define FONCTION2_H_
+
+
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+#include <SDL/SDL_image.h>
+
+
+typedef struct 
+{
+
+SDL_Surface *map;
+
+SDL_Rect posmap;
+
+}ma;
+
+typedef struct 
+{
+
+SDL_Surface *mini;
+
+SDL_Rect posmini;
+
+}mi;
+
+mi minimap(perso p,mi m2);
+
+#endif
